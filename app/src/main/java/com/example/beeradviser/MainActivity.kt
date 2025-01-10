1f0346409819b90bb7cc228f484c17ef09107c72
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-10 14:05:31 +0400

    feat: findBeer Listener added

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.beeradviser
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = "Beer color is $color"
+        }
     }
 }
